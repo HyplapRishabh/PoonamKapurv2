ffcd1a394bfe64dbec21a8405d3188d5283dbc42
Zaid-Hyplap <zaid.sayed@hyplap.com>
2022-12-30 15:21:29 +0530

    fixed few ui

--- resources/views/admin/orders/packageFailed.blade.php
+++ resources/views/admin/orders/packageFailed.blade.php
@@ -57,7 +57,6 @@
                             <th>Reason</th>
                             <th>Message</th>
                             <th>Date</th>
-                            <th>Action</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -83,48 +82,7 @@
                             <td class="align-middle text-center">{{$data->reason}}
                             <td class="align-middle text-center">{{$data->errormsg}}
                             <td class="align-middle text-center">{{date('d M, Y h:i a', strtotime($data->created_at))}}
-                            
-                            <td class="table-action text-center">
-                                <div>
-                                    <a href="" class="btn btn-outline-warning has-ripple" data-toggle="modal" data-target="#viewModal{{$data->id}}"><i class="fas fa-eye"></i>View Orders</a>
-                                    <!-- <a href="" class="btn btn-icon btn-outline-danger has-ripple" data-toggle="modal" data-target="#deleteModal{{$data->id}}"><i class="far fa-trash-alt"></i></a> -->
-                                </div>
-                            </td>
                         </tr>
-                        <!--View Modal -->
-                        <div class="modal fade" id="viewModal{{$data->id}}" data-backdrop="static" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                            <div class="modal-dialog modal-xl">
-                                <div class="modal-content">
-                                    <div class="modal-header">
-                                        <h5 class="modal-title" style="font-weight: 600; color: black; font-size: large;">Update Order</h5>
-                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                            <i class="fas fa-times" style="font-size: 25px; "></i>
-                                        </button>
-                                    </div>
-                                    <div class="modal-body" style="color: black;">
-                                        <div class="row">
-                                            <div class="col-3 text-center" style="border: 1px solid #000;">Product</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">Price</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">Quantity</div>
-                                            <div class="col-3 text-center" style="border: 1px solid #000;">Addon</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">Price</div>
-                                        </div>
-                                        @foreach($data->trxalacartorder as $key=>$orderdetails)
-                                        <div class="row">
-                                            <div class="col-3 text-center" style="border: 1px solid #000;">{{$orderdetails->productName}}</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">{{$orderdetails->productPrice}}</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">{{$orderdetails->qty}}</div>
-                                            <div class="col-3 text-center" style="border: 1px solid #000;">{{$orderdetails->addonName}}</div>
-                                            <div class="col-2 text-center" style="border: 1px solid #000;">{{$orderdetails->addonprice}}</div>
-                                        </div>
-
-                                        @endforeach
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <!-- End View Modal -->
                         @endforeach
                     </tbody>
                 </table>
